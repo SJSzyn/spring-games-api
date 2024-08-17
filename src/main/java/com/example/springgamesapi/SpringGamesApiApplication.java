277cf12e5c69712e8dfb550381e5fc9a5204c95c
SJ <serge.szynkarski@le-campus-numerique.fr>
2024-08-17 15:16:50 +0200

    first commit

---
+++ src/main/java/com/example/springgamesapi/SpringGamesApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.springgamesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGamesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGamesApiApplication.class, args);
+    }
+
+}
